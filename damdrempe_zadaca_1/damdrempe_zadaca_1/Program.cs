43a4919c062b4eb70c3b4c53a1009062b08e9fca
Damir Drempetic <damir.drempetic@live.com>
2018-11-01 16:42:38 +0100

    Added singleton class for loading parameters from file

--- damdrempe_zadaca_1/damdrempe_zadaca_1/Program.cs
+++ damdrempe_zadaca_1/damdrempe_zadaca_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace damdrempe_zadaca_1
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Broj argumenata mora biti jednak 1!");
+            }
+
+            string nazivDatotekeParametara = args[0];
+            SingletonParametri singletonParametri = SingletonParametri.DohvatiInstancu(nazivDatotekeParametara);
+            Console.WriteLine(singletonParametri.DohvatiParametar("spremnici"));
+
+            Console.Read();
         }
     }
 }
---
+++ damdrempe_zadaca_1/damdrempe_zadaca_1/SingletonParametri.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace damdrempe_zadaca_1
+{
+    public interface IParametri
+    {
+        string DohvatiParametar(string nazivParametra);
+    }
+
+    class SingletonParametri : IParametri
+    {
+        private static SingletonParametri instanca;
+
+        private Dictionary<string, string> parametri = new Dictionary<string, string>();
+
+        private SingletonParametri(string nazivDatotekeParametara)
+        {
+            Console.WriteLine("Pokrenuto ucitavanje parametara iz datoteke...");
+
+            string[] retci = File.ReadAllLines(nazivDatotekeParametara);
+            foreach (string redak in retci)
+            {
+                string[] rezultat = redak.Split(':');
+                parametri.Add(rezultat[0], rezultat[1]);
+            }
+        }
+
+        public string DohvatiParametar(string nazivParametra)
+        {
+            return parametri[nazivParametra];
+        }     
+
+        public static SingletonParametri DohvatiInstancu(string nazivDatotekeParametara)
+        {
+            if (instanca == null)
+            {
+                instanca = new SingletonParametri(nazivDatotekeParametara);
+            }
+
+            return instanca;
+        }
+    }
+}
