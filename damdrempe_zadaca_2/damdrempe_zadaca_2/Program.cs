87d2d90f0c052e3b479f3e45be439b9fb2f890d8
Damir Drempetic <damir.drempetic@live.com>
2018-11-27 17:43:40 +0100

    HW2 Refactored main method

--- damdrempe_zadaca_2/damdrempe_zadaca_2/Program.cs
+++ damdrempe_zadaca_2/damdrempe_zadaca_2/Program.cs
@@ -11,9 +11,18 @@ namespace damdrempe_zadaca_2
 {
     class Program
     {
-        public static string DatotekaParametara;
-        public static string PutanjaDatoteka;
         public static IspisivacSingleton Ispisivac = IspisivacSingleton.DohvatiInstancu();
+
+        public static string DatotekaParametara;
+        public static ParametriSingleton Parametri;
+        public static string PutanjaDatoteka;
+
+        public static List<Ulica> PripremljeneUlice = new List<Ulica>();
+        public static List<Spremnik> PripremljeniSpremnici = new List<Spremnik>();
+
+        public static List<Ulica> Ulice = new List<Ulica>();
+        public static List<Spremnik> Spremnici = new List<Spremnik>();
+        public static List<Podrucje> Podrucja = new List<Podrucje>();
 
         static void Main(string[] args)
         {
@@ -21,47 +30,9 @@ namespace damdrempe_zadaca_2
             {
                 Pomocno.ZavrsiProgram("Broj argumenata mora biti jednak 1.", false);
             }
+            DatotekaParametara = args[0];            
 
-            DatotekaParametara = args[0];
-            if(!File.Exists(DatotekaParametara))
-            {
-                Pomocno.ZavrsiProgram("Datoteka s parametrima ne postoji!", false);
-            }
-            ParametriSingleton parametri = ParametriSingleton.DohvatiInstancu(DatotekaParametara);
-            PutanjaDatoteka = Path.GetDirectoryName(DatotekaParametara);
-
-            string datotekaUlice = Pomocno.DohvatiPutanjuDatoteke(parametri.DohvatiParametar("ulice"));
-            Popis ulicaPopis = new UlicaPopis();
-            List<Redak> ulicaPopisRetci = ulicaPopis.UcitajRetke(datotekaUlice);
-
-            string datotekaSpremnika = Pomocno.DohvatiPutanjuDatoteke(parametri.DohvatiParametar("spremnici"));
-            Popis spremnikPopis = new SpremnikPopis();
-            List<Redak> spremnikPopisRetci = spremnikPopis.UcitajRetke(datotekaSpremnika);
-
-            string datotekaVozila = Pomocno.DohvatiPutanjuDatoteke(parametri.DohvatiParametar("vozila"));
-            Popis voziloPopis = new VoziloPopis();
-            List<Redak> voziloPopisRetci = voziloPopis.UcitajRetke(datotekaVozila);
-
-            List<Ulica> pripremljeneUlice = PripremateljPrototype.PripremiUlice(ulicaPopisRetci.Cast<UlicaRedak>().ToList());
-            List<Spremnik> pripremljeniSpremnici = PripremateljPrototype.PripremiSpremnike(spremnikPopisRetci.Cast<SpremnikRedak>().ToList());
-
-            List<Ulica> ulice = GeneratorEntiteta.StvoriKorisnike(pripremljeneUlice);
-            List<Spremnik> spremnici = GeneratorEntiteta.StvoriSpremnike(pripremljeneUlice, pripremljeniSpremnici);
-
-            ulice = InicijalizatorOtpada.OdrediOtpadKorisnicima(ulice, DatotekaParametara);
-            spremnici = InicijalizatorOtpada.OdloziOtpadKorisnika(ulice, spremnici);
-                        
-            InicijalizatorOtpada.IspisiOtpadPoUlicama(ulice);
-
-            //testPodrucja();
-
-            string datotekaPodrucja = Pomocno.DohvatiPutanjuDatoteke(parametri.DohvatiParametar("područja"));
-            Popis podrucjePopis = new PodrucjePopis();
-            List<PodrucjeRedak> podrucjaPopisRetci = podrucjePopis.UcitajRetke(datotekaPodrucja).Cast<PodrucjeRedak>().ToList();
-
-            List<Podrucje> podrucja = PripremateljPodrucja.PripremiPodrucja(podrucjaPopisRetci);
-
-
+            InicijalizatorSustava.Pokreni();     
             Pomocno.ZavrsiProgram("Program izvrsen do kraja.", true);
         }
     }
--- damdrempe_zadaca_2/damdrempe_zadaca_2/Sustav/InicijalizatorSustava.cs
+++ damdrempe_zadaca_2/damdrempe_zadaca_2/Sustav/InicijalizatorSustava.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using damdrempe_zadaca_2.Citaci;
+using damdrempe_zadaca_2.Podaci.Modeli;
+using damdrempe_zadaca_2.Pomagaci.Entiteti;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static damdrempe_zadaca_2.Pomagaci.Entiteti.PodrucjaComposite;
 
 namespace damdrempe_zadaca_2.Sustav
 {
     class InicijalizatorSustava
     {
+
+        public static void Pokreni()
+        {
+            UcitajParametre();
+            UcitajZapiseIzDatoteka();
+            StvoriKonacnePodatkeSustava();
+            StvoriOtpad();
+
+            InicijalizatorOtpada.IspisiOtpadPoUlicama(Program.Ulice);
+        }
+
+        private static void UcitajParametre()
+        {
+            if (!File.Exists(Program.DatotekaParametara))
+            {
+                Pomocno.ZavrsiProgram("Datoteka s parametrima ne postoji!", false);
+            }
+            Program.Parametri = ParametriSingleton.DohvatiInstancu(Program.DatotekaParametara);
+            Program.PutanjaDatoteka = Path.GetDirectoryName(Program.DatotekaParametara);
+        }
+
+        private static void UcitajZapiseIzDatoteka()
+        {
+            string datotekaUlice = Pomocno.DohvatiPutanjuDatoteke(Program.Parametri.DohvatiParametar("ulice"));
+            Popis ulicaPopis = new UlicaPopis();
+            List<Redak> ulicaPopisRetci = ulicaPopis.UcitajRetke(datotekaUlice);
+
+            string datotekaSpremnika = Pomocno.DohvatiPutanjuDatoteke(Program.Parametri.DohvatiParametar("spremnici"));
+            Popis spremnikPopis = new SpremnikPopis();
+            List<Redak> spremnikPopisRetci = spremnikPopis.UcitajRetke(datotekaSpremnika);
+
+            string datotekaVozila = Pomocno.DohvatiPutanjuDatoteke(Program.Parametri.DohvatiParametar("vozila"));
+            Popis voziloPopis = new VoziloPopis();
+            List<Redak> voziloPopisRetci = voziloPopis.UcitajRetke(datotekaVozila);
+
+            string datotekaPodrucja = Pomocno.DohvatiPutanjuDatoteke(Program.Parametri.DohvatiParametar("područja"));
+            Popis podrucjePopis = new PodrucjePopis();
+            List<PodrucjeRedak> podrucjaPopisRetci = podrucjePopis.UcitajRetke(datotekaPodrucja).Cast<PodrucjeRedak>().ToList();            
+
+            Program.PripremljeneUlice = PripremateljPrototype.PripremiUlice(ulicaPopisRetci.Cast<UlicaRedak>().ToList());
+            Program.PripremljeniSpremnici = PripremateljPrototype.PripremiSpremnike(spremnikPopisRetci.Cast<SpremnikRedak>().ToList());
+            Program.Podrucja = PripremateljPodrucja.PripremiPodrucja(podrucjaPopisRetci);
+        }
+
+        private static void StvoriKonacnePodatkeSustava()
+        {
+            Program.Ulice = GeneratorEntiteta.StvoriKorisnike(Program.PripremljeneUlice);
+            Program.Spremnici = GeneratorEntiteta.StvoriSpremnike(Program.PripremljeneUlice, Program.PripremljeniSpremnici);            
+        }
+
+        private static void StvoriOtpad()
+        {
+            Program.Ulice = InicijalizatorOtpada.OdrediOtpadKorisnicima(Program.Ulice, Program.DatotekaParametara);
+            Program.Spremnici = InicijalizatorOtpada.OdloziOtpadKorisnika(Program.Ulice, Program.Spremnici);
+        }
+
     }
 }
